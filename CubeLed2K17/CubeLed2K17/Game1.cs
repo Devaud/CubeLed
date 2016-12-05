f92c368bf934c53c1ce4eb5f261a4fe0952c76ce
kevinamado <kevin-amado@hotmail.com>
2016-12-05 16:06:05 +0100

    Modification monogame

--- CubeLed2K17/CubeLed2K17/Game1.cs
+++ CubeLed2K17/CubeLed2K17/Game1.cs
@@ -12,6 +12,23 @@ namespace CubeLed2K17
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        //Camera
+        Vector3 camTarget;
+        Vector3 camPosition;
+        Matrix projectionMatrix;
+        Matrix viewMatrix;
+        Matrix worldMatrix;
+
+        //BasicEffect for rendering
+        BasicEffect basicEffect;
+
+        //Geometric info
+        VertexPositionColor[] triangleVertices;
+        VertexBuffer vertexBuffer;
+
+        //Orbit
+        bool orbit = false;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -26,10 +43,47 @@ namespace CubeLed2K17
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
-        }
+
+            //Setup Camera
+            camTarget = new Vector3(0f, 0f, 0f);
+            camPosition = new Vector3(0f, 0f, -100f);
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+                               MathHelper.ToRadians(45f), 
+                               GraphicsDevice.DisplayMode.AspectRatio,
+                1f, 1000f);
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget, 
+                         new Vector3(0f, 1f, 0f));// Y up
+            worldMatrix = Matrix.CreateWorld(camTarget, Vector3.
+                          Forward, Vector3.Up);
+
+            //BasicEffect
+            basicEffect = new BasicEffect(GraphicsDevice);
+            basicEffect.Alpha = 1f;
+
+            // Want to see the colors of the vertices, this needs to be on
+            basicEffect.VertexColorEnabled = true;
+
+            //Lighting requires normal information which VertexPositionColor does not have
+            //If you want to use lighting and VPC you need to create a custom def
+            basicEffect.LightingEnabled = false;
+
+            //Geometry  - a simple triangle about the origin
+            triangleVertices = new VertexPositionColor[3];
+            triangleVertices[0] = new VertexPositionColor(new Vector3(
+                                  0, 20, 0), Color.Red);
+            triangleVertices[1] = new VertexPositionColor(new Vector3(-
+                                  20, -20, 0), Color.Green);
+            triangleVertices[2] = new VertexPositionColor(new Vector3(
+                                  20, -20, 0), Color.Blue);
+
+            //Vert buffer
+            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(
+                           VertexPositionColor), 3, BufferUsage.
+                           WriteOnly);
+            vertexBuffer.SetData<VertexPositionColor>(triangleVertices)
+                                                      ;
+        }       
 
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
@@ -59,11 +113,53 @@ namespace CubeLed2K17
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back ==
+                            ButtonState.Pressed || Keyboard.GetState().IsKeyDown(
+                            Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                camPosition.X -= 1f;
+                camTarget.X -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                camPosition.X += 1f;
+                camTarget.X += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                camPosition.Y -= 1f;
+                camTarget.Y -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                camPosition.Y += 1f;
+                camTarget.Y += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemPlus))
+            {
+                camPosition.Z += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemMinus))
+            {
+                camPosition.Z -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                orbit = !orbit;
+            }
 
+            if (orbit)
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationY(
+                                        MathHelper.ToRadians(1f));
+                camPosition = Vector3.Transform(camPosition,
+                              rotationMatrix);
+            }
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,
+                         Vector3.Up);
             base.Update(gameTime);
         }
 
@@ -73,10 +169,26 @@ namespace CubeLed2K17
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
+            basicEffect.Projection = projectionMatrix;
+            basicEffect.View = viewMatrix;
+            basicEffect.World = worldMatrix;
+
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
 
-            // TODO: Add your drawing code here
+            //Turn off culling so we see both sides of our rendered triangle
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
 
+            foreach(EffectPass pass in basicEffect.CurrentTechnique.
+                    Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawPrimitives(PrimitiveType.
+                                              TriangleList, 0, 3);
+            }
+            
             base.Draw(gameTime);
         }
     }
